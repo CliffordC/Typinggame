f7f402bf98d6b1dae341aaa0d3f747033ac28a27
Clifford Chirwa <siestaftw10@gmail.com>
2024-02-11 01:25:21 -0800

    Got the keyboard to change background colors on keypress, with a few bugs of course
    Bugs: - Background color stays stuck after repeated button pressing.
    - TypeError on keymapKey object for spacebar present in console.
    - A few more to be discovered.

--- src/script.js
+++ src/script.js
@@ -46,37 +46,74 @@ const qbg = (keymapKey[0].dataset.key)
  * the corresponding span can be programmed to change.
  */
 
-
+//Collection of keys and classlist to manipulate
 const letterDict = {
     'q':keymapKey[0].classList,
+    'w':keymapKey[1].classList,
+    'e':keymapKey[2].classList,
+    'r':keymapKey[3].classList,
+    't':keymapKey[4].classList,
+    'y':keymapKey[5].classList,
+    'u':keymapKey[6].classList,
+    'i':keymapKey[7].classList,
+    'o':keymapKey[8].classList,
+    'p':keymapKey[9].classList,
+    '[':keymapKey[10].classList,
+    ']':keymapKey[11].classList,
+    'a':keymapKey[12].classList,
+    's':keymapKey[13].classList,
+    'd':keymapKey[14].classList,
+    'f':keymapKey[15].classList,
+    'g':keymapKey[16].classList,
+    'h':keymapKey[17].classList,
+    'j':keymapKey[18].classList,
+    'k':keymapKey[19].classList,
+    'l':keymapKey[20].classList,
+    ';':keymapKey[21].classList,
+    "'":keymapKey[22].classList,
+    'z':keymapKey[23].classList,
+    'x':keymapKey[24].classList,
+    'c':keymapKey[25].classList,
+    'v':keymapKey[26].classList,
+    'b':keymapKey[27].classList,
+    'n':keymapKey[28].classList,
+    'm':keymapKey[29].classList,
+    ',':keymapKey[30].classList,
+    '.':keymapKey[31].classList,
+    '/':keymapKey[32].classList,
+    '_':keymapKey[33].classList
 }
 
+const keyPressIndicator = (keyPress) => {
+    letterDict[keyPress].toggle('pressed');
+}
 
 document.addEventListener("keydown",
     (e)=> {
-        if(e.defaultPrevented){
-            return;
+        e.preventDefault();
+        let btn = e.key.toLowerCase();
+
+        if(e.keyCode === 32){
+            keyPressIndicator('_');
         }
-        let btn = e.key;
-        
-        console.log(btn);
-        if(btn==='q'){
-            letterDict[btn].toggle('pressed');
+        else if(65< e.keyCode<=90){
+            keyPressIndicator(btn);
         }
 
 });
 
 document.addEventListener("keyup",
     (e)=> {
-        if(e.defaultPrevented){
-            return;
-        }
+       // if(e.defaultPrevented){
+      //      return;
+       // }
         let btn = e.key;
-        
-        console.log(btn);
-        if(btn==='q'){
-            letterDict[btn].toggle('pressed');
+       
+        if(e.keyCode === 32){
+            e.preventDefault();
+            keyPressIndicator('_');
         }
+        keyPressIndicator(btn);
 });
 
 
